df02616a79f9e35a3408f0ac4c887d892a656186
Burcu Bayraö <burju7@yahoo.com>
2023-06-12 09:47:06 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let reversed = word.split("").reverse().join("");
+  return word === reversed;
 }
 
 /* 
   Add your pseudocode here
+  split the word its letter
+  Reversed and order in the same order
+  Check word is same previous one
+
 */
 
 /*
   Add written explanation of your solution here
+  First word is needed to be split to its letter
+  Reversed and order in the same order
+  Check word is same previous one
 */
 
 // You can run `node index.js` to view these console logs
